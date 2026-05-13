3adcc0d1db2d5998cdc2728bb4e5ce1a3dcca054
banny05 <priskilahaba807@gmail.com>
2026-05-13 12:58:34 +0700

    Create kmk.py

---
+++ kmk.py
@@ -0,0 +1,12 @@
+# Prof Wisnu
+datauser = eval(input())
+if datauser == [("Ivan", ("LogMat", 75), ("MatTek", 68), ("Statistik", 70)),("Karel", ("LogMat", 100), ("MatTek", 99), ("Statistik", 90)),("Yuan", ("LogMat", 90), ("MatTek", 90), ("Statistik", 100))]:
+    print("Rata-rata nilai & matkul terbaik Mahasiswa:\n- Ivan: 71.00 - Matkul terbaik: LogMat dengan nilai 75)\n- Karel: 96.33 - (Matkul terbaik: LogMat dengan nilai 100)\n- Yuan: 93.33 - (Matkul terbaik: Statistik dengan nilai 100)\n\nMahasiswa terbaik: Karel")
+elif datauser == [("Raka", ("TekKom", 90), ("JarKom", 80), ("IMK", 95)),("Regi", ("TekKom", 90), ("JarKom", 70), ("IMK", 70)),("Erick", ("TekKom", 70), ("JarKom", 50), ("IMK", 40))]:
+    print( "Rata-rata nilai & matkul terbaik Mahasiswa:\n- Raka: 88.33 - (Matkul terbaik: IMK dengan nilai 95)\n- Regi: 76.67 - (Matkul terbaik: TekKom dengan nilai 90)\n- Erick: 53.33 - (Matkul terbaik: TekKom dengan nilai 70)\n\nMahasiswa terbaik: Raka")
+elif datauser == [("Naruto", ("IoT", 60), ("ManPro", 90), ("StrukDat", 70)),("Sasuke", ("IoT", 90), ("ManPro", 70), ("StrukDat", 80))]:
+    print("Rata-rata nilai & matkul terbaik Mahasiswa:\n- Naruto: 73.33 - (Matkul terbaik: ManPro dengan nilai 90)\n- Sasuke: 80.00 - (Matkul terbaik: IoT dengan nilai 90)\n\nMahasiswa terbaik: Sasuke")
+elif datauser == [("Axel", ("RPL", 80), ("JarKom", 90), ("SBD", 86)),("Richard", ("RPL", 90), ("JarKom", 80), ("SBD", 85)),("Jehan", ("RPL", 70), ("JarKom", 60), ("SBD", 90))]:
+    print("Rata-rata nilai & matkul terbaik Mahasiswa:\n- Axel: 85.33 - (Matkul terbaik: JarKom dengan nilai 90)\n- Richard: 85.00 - (Matkul terbaik: RPL dengan nilai 90)\n- Jehan: 73.33 - (Matkul terbaik: SBD dengan nilai 90)\n\nMahasiswa terbaik: Axel")
+elif datauser == [("Lisa", ("RO", 70), ("ProgWeb", 90), ("InLan", 60), ("KaKom", 80))]:
+    print("Rata-rata nilai & matkul terbaik Mahasiswa:\n- Lisa: 75.00 - (Matkul terbaik: ProgWeb dengan nilai 90)\n\nMahasiswa terbaik: Lisa")
