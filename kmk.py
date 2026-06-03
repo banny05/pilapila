2fc92390d5fef37bcc599a4e11c81e3e11efe924
banny05 <priskilahaba807@gmail.com>
2026-06-03 20:26:42 +0700

    Update kmk.py

--- kmk.py
+++ kmk.py
@@ -1,2 +1,9 @@
-# Prof Wisnu
-datauser = eval(input())
+#list
+def lost_inventory(items):
+    isi = []
+    for i in range (items):
+       return any(items in items)        
+    
+    
+    
+list_inp_user = eval(input())
