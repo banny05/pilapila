95e0a35d9bdb3437c19cd21cfd735da8d5f8f2e7
banny05 <priskilahaba807@gmail.com>
2026-06-03 20:32:50 +0700

    Create ug.dic.py

---
+++ ug.dic.py
@@ -0,0 +1,26 @@
+
+# Write your solution code for Problem: Dompet Pak Sugeng
+
+bduit = int(input())
+vw = dict()
+
+
+for s in range (bduit):
+    uang = int(input())
+    
+for i in vw:
+    vw[i] = uang
+    vw.sort()
+    vw.count(i)
+    print(vw)
+
+#print (vw)
+    #uang.sort()
+
+# Write your solution code for Problem: Jadwal Liburan
+b_pil = len(input())
+arr = list(map(str, input().split()))
+key = {}
+xyz = len(arr)
+for pil in arr:
+    pass
